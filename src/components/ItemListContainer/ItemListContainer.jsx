5dfbcb75d6e3859bbd44db9da942c5fdbdfc96b7
FranciscoHellbusch <fghellbusch@gmail.com>
2023-09-08 12:01:38 -0300

    manejo de estados y async await con react

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,6 +1,22 @@
+import { useState } from "react";
+
 // eslint-disable-next-line react/prop-types
 const ItemListContainer = ({ greeting }) => {
-  return <>{greeting}</>;
+  const [result, setResult] = useState([])
+  // let resultadoSinState = []
+
+  const llamadaALaApi = async () => {
+    const resultado = await fetch('https://pokeapi.co/api/v2/ability/')
+    const resJson = await resultado.json()
+    console.log(resJson.results)
+    setResult(resJson.results)
+  }
+  
+  return <>
+    {greeting}
+    <button onClick={llamadaALaApi}>llamar a la api</button>
+    {result.map((elemento, index) => <h1 key={index}>{elemento.name}</h1> )}
+  </>;
 };
 
 export default ItemListContainer;
